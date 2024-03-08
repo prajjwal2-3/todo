6c1046b9b26bc40087ca961ba98004baf4ff629c
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-08 21:16:05 +0530

    done

--- server/index.js
+++ server/index.js
@@ -8,9 +8,9 @@ app.use(cors())
 app.use(bodyParser.json());
 
 let todos = [
-    {id:1,text:'learn react',done:false},
-    {id:2,text:'learn express',done:false},
-    {id:3,text:'learn mongodb',done:false}
+    // {id:1,text:'learn react',done:false},
+    // {id:2,text:'learn express',done:false},
+    // {id:3,text:'learn mongodb',done:false}
 
 ];
 
@@ -26,13 +26,13 @@ app.post('/api/todos',(req,res)=>{
 app.put('/api/todos/:id',(req,res)=>{
     const id = parseInt(req.params.id);
     const updatedtodo = req.body;
-    todosnew = todos.map(todo=>(todo.id===id?updatedtodo:todo))
-    res.json(todosnew)
+   todos =   todos.map(todo=>(todo.id===id?updatedtodo:todo))
+    res.json(todos)
 })
 app.delete('/api/todos/:id',(req,res)=>{
     const id = parseInt(req.params.id);
-    todosnew2 = todos.filter(todo=>todo.id!==id);
-    res.json(todosnew2)
+   todos = todos.filter(todo=>todo.id!==id);
+    res.json(todos)
 })
 app.listen(port,()=>{
     console.log('server is running')
